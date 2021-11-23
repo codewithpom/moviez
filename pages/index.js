05d11d1977eaf1d3c3cdd79d0d2c1874311b7534
Padmashree Jha <codewithpom@gmail.com>
2021-11-23 21:51:29 +0530

    Added meta tags for SEO optimization

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,13 @@ export default function Home() {
       <Head>
         <title>Search for any movie</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="content-language" content="en" />
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="movie, search, random" />
+        <meta name="author" content="Padmashree Jha" />
+        <meta name="publisher" content="Padmashree Jha" />
+        <meta name="description" content="Search any movie on the internet" />
       </Head>
       <Form func={setMovies} /> {/* Form here */}
       <br />
