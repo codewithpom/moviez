e8aad1063a837811e4ae2650bee0983889343673
Padmashree Jha <codewithpom@gmail.com>
2021-11-23 21:47:54 +0530

    Changed title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState,useEffect } from "react"
+import { useState, useEffect } from "react"
 import Form from '../components/Form'
 import Table from '../components/Table';
 
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="container pb-5">
       <Head>
-        <title>Create Next App</title>
+        <title>Search for any movie</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Form func={setMovies} /> {/* Form here */}
