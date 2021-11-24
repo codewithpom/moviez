4df3eee2e516f3050ee2eae227be77f923bcb281
Padmashree Jha <codewithpom@gmail.com>
2021-11-24 12:25:52 +0530

    Cleaned code

---
+++ components/seo/index.js
@@ -0,0 +1,22 @@
+export default function Seo() {
+    return (
+        <>
+            <title>Search for any movie</title>
+            <link rel={"icon"} href={"/favicon.ico"} />
+            <meta http-equiv={"content-language"} content={"en"} />
+            <meta charset={"UTF-8"} />
+            <meta name={"viewport"} content={"width=device-width, initial-scale=1.0"} />
+            <meta name={"keywords"} content={"movie, search, random"} />
+            <meta name={"author"} content={"Padmashree Jha"} />
+            <meta name={"publisher"} content={"Padmashree Jha"} />
+            <meta name={"description"} content={"Search any movie on the internet"} />
+            <meta name={"copyright"} content={"Padmashree Jha"} />
+            <meta name={"robots"} content={"index, follow"} />
+            <meta property={"og:description"} content={"Search for any movie on the internet"} />
+            <meta property={"og:title"} content={"Search for any movie on the internet"} />
+            <meta property={"og:type"} content={"website"} />
+            <meta property={"og:image"} content={"https://image.tmdb.org/t/p/original//78lPtwv72eTNqFW9COBYI0dWDJa.jpg"} />
+            <meta property={"og:url"} content={"http://moviez-nu.vercel.app/"} />
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import { useState, useEffect } from "react"
-import Form from '../components/Form'
-import Table from '../components/Table';
+import Form from '../components/parts/Form'
+import Table from '../components/parts/Table';
+import Seo from '../components/seo';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
@@ -10,22 +11,9 @@ export default function Home() {
   return (
     <div className="container pb-5">
       <Head>
-        <title>Search for any movie</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta http-equiv="content-language" content="en" />
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="keywords" content="movie, search, random" />
-        <meta name="author" content="Padmashree Jha" />
-        <meta name="publisher" content="Padmashree Jha" />
-        <meta name="description" content="Search any movie on the internet" />
-        <meta name="copyright" content="Padmashree Jha" />
-        <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="Search for any movie" />
-        <meta property={"og:type"} content={"website"} />
-        <meta property={"og:image"} content={"https://image.tmdb.org/t/p/original//78lPtwv72eTNqFW9COBYI0dWDJa.jpg"} />
-
+        <Seo />
       </Head>
+
       <Form func={setMovies} /> {/* Form here */}
       <br />
       <Table movies={movies} /> {/* Table here */}
